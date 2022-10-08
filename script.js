5f46948c1e7c62ec0e9ca5969be1189f81d223ed
Kera <106094452+Keremesh@users.noreply.github.com>
2022-10-09 02:44:07 +0600

    bigger buttons, added Enter

--- script.js
+++ script.js
@@ -4,243 +4,251 @@ let display = document.getElementById("display");
 let tempTxt = "";
 
 let A = document.getElementById("A");
-A.onclick = function () {
+A.onmousedown = function () {
   display.innerHTML = tempTxt + "A";
   tempTxt = tempTxt + "A";
 };
 
 let B = document.getElementById("B");
-B.onclick = function () {
+B.onmousedown = function () {
   display.innerHTML = tempTxt + "B";
   tempTxt = tempTxt + "B";
 };
 
 let C = document.getElementById("C");
-C.onclick = function () {
+C.onmousedown = function () {
   display.innerHTML = tempTxt + "C";
   tempTxt = tempTxt + "C";
 };
 
 let D = document.getElementById("D");
-D.onclick = function () {
+D.onmousedown = function () {
   display.innerHTML = tempTxt + "D";
   tempTxt = tempTxt + "D";
 };
 let E = document.getElementById("E");
-E.onclick = function () {
+E.onmousedown = function () {
   display.innerHTML = tempTxt + "E";
   tempTxt = tempTxt + "E";
 };
 
 let F = document.getElementById("F");
-F.onclick = function () {
+F.onmousedown = function () {
   display.innerHTML = tempTxt + "F";
   tempTxt = tempTxt + "F";
 };
 
 let G = document.getElementById("G");
-G.onclick = function () {
+G.onmousedown = function () {
   display.innerHTML = tempTxt + "G";
   tempTxt = tempTxt + "G";
 };
 
 let H = document.getElementById("H");
-H.onclick = function () {
+H.onmousedown = function () {
   display.innerHTML = tempTxt + "H";
   tempTxt = tempTxt + "H";
 };
 
 let I = document.getElementById("I");
-I.onclick = function () {
+I.onmousedown = function () {
   display.innerHTML = tempTxt + "I";
   tempTxt = tempTxt + "I";
 };
 
 let J = document.getElementById("J");
-J.onclick = function () {
+J.onmousedown = function () {
   display.innerHTML = tempTxt + "J";
   tempTxt = tempTxt + "J";
 };
 
 let K = document.getElementById("K");
-K.onclick = function () {
+K.onmousedown = function () {
   display.innerHTML = tempTxt + "K";
   tempTxt = tempTxt + "K";
 };
 
 let L = document.getElementById("L");
-L.onclick = function () {
+L.onmousedown = function () {
   display.innerHTML = tempTxt + "L";
   tempTxt = tempTxt + "L";
 };
 
 let M = document.getElementById("M");
-M.onclick = function () {
+M.onmousedown = function () {
   display.innerHTML = tempTxt + "M";
   tempTxt = tempTxt + "M";
 };
 
 let N = document.getElementById("N");
-N.onclick = function () {
+N.onmousedown = function () {
   display.innerHTML = tempTxt + "N";
   tempTxt = tempTxt + "N";
 };
 
 let O = document.getElementById("O");
-O.onclick = function () {
+O.onmousedown = function () {
   display.innerHTML = tempTxt + "O";
   tempTxt = tempTxt + "O";
 };
 
 let P = document.getElementById("P");
-P.onclick = function () {
+P.onmousedown = function () {
   display.innerHTML = tempTxt + "P";
   tempTxt = tempTxt + "P";
 };
 
 let Q = document.getElementById("Q");
-Q.onclick = function () {
+Q.onmousedown = function () {
   display.innerHTML = tempTxt + "Q";
   tempTxt = tempTxt + "Q";
 };
 
 let R = document.getElementById("R");
-R.onclick = function () {
+R.onmousedown = function () {
   display.innerHTML = tempTxt + "R";
   tempTxt = tempTxt + "R";
 };
 
 let S = document.getElementById("S");
-S.onclick = function () {
+S.onmousedown = function () {
   display.innerHTML = tempTxt + "S";
   tempTxt = tempTxt + "S";
 };
 
 let T = document.getElementById("T");
-T.onclick = function () {
+T.onmousedown = function () {
   display.innerHTML = tempTxt + "T";
   tempTxt = tempTxt + "T";
 };
 
 let U = document.getElementById("U");
-U.onclick = function () {
+U.onmousedown = function () {
   display.innerHTML = tempTxt + "U";
   tempTxt = tempTxt + "U";
 };
 
 let V = document.getElementById("V");
-V.onclick = function () {
+V.onmousedown = function () {
   display.innerHTML = tempTxt + "V";
   tempTxt = tempTxt + "V";
 };
 
 let W = document.getElementById("W");
-W.onclick = function () {
+W.onmousedown = function () {
   display.innerHTML = tempTxt + "W";
   tempTxt = tempTxt + "W";
 };
 
 let X = document.getElementById("X");
-X.onclick = function () {
+X.onmousedown = function () {
   display.innerHTML = tempTxt + "X";
   tempTxt = tempTxt + "X";
 };
 
 let Y = document.getElementById("Y");
-Y.onclick = function () {
+Y.onmousedown = function () {
   display.innerHTML = tempTxt + "Y";
   tempTxt = tempTxt + "Y";
 };
 
 let Z = document.getElementById("Z");
-Z.onclick = function () {
+Z.onmousedown = function () {
   display.innerHTML = tempTxt + "Z";
   tempTxt = tempTxt + "Z";
 };
 
 //Numbers
 let n1 = document.getElementById("1");
-n1.onclick = function () {
+n1.onmousedown = function () {
   display.innerHTML = tempTxt + "1";
   tempTxt = tempTxt + "1";
 };
 
 let n2 = document.getElementById("2");
-n2.onclick = function () {
+n2.onmousedown = function () {
   display.innerHTML = tempTxt + "2";
   tempTxt = tempTxt + "2";
 };
 
 let n3 = document.getElementById("3");
-n3.onclick = function () {
+n3.onmousedown = function () {
   display.innerHTML = tempTxt + "3";
   tempTxt = tempTxt + "3";
 };
 let n4 = document.getElementById("4");
-n4.onclick = function () {
+n4.onmousedown = function () {
   display.innerHTML = tempTxt + "4";
   tempTxt = tempTxt + "4";
 };
 
 let n5 = document.getElementById("5");
-n5.onclick = function () {
+n5.onmousedown = function () {
   display.innerHTML = tempTxt + "5";
   tempTxt = tempTxt + "5";
 };
 
 let n6 = document.getElementById("6");
-n6.onclick = function () {
+n6.onmousedown = function () {
   display.innerHTML = tempTxt + "6";
   tempTxt = tempTxt + "6";
 };
 
 let n7 = document.getElementById("7");
-n7.onclick = function () {
+n7.onmousedown = function () {
   display.innerHTML = tempTxt + "7";
   tempTxt = tempTxt + "7";
 };
 
 let n8 = document.getElementById("8");
-n8.onclick = function () {
+n8.onmousedown = function () {
   display.innerHTML = tempTxt + "8";
   tempTxt = tempTxt + "8";
 };
 let n9 = document.getElementById("9");
-n9.onclick = function () {
+n9.onmousedown = function () {
   display.innerHTML = tempTxt + "9";
   tempTxt = tempTxt + "9";
 };
 
 // Space
 let spacebar = document.getElementById("spacebar");
-spacebar.onclick = function () {
+spacebar.onmousedown = function () {
+  display.innerHTML = tempTxt + " ";
+  tempTxt += " ";
+};
+
+// Enter
+let enter = document.getElementById("enter");
+enter.onmousedown = function () {
   display.innerHTML = tempTxt + "\n";
   tempTxt += "\n";
 };
+
 // let spacebar = document.getElementById("spacebar");
-// spacebar.onclick = function () {
+// spacebar.onmousedown = function () {
 // display.innerHTML = tempTxt + ' ';
 // };
 
 // Backspace
 // let backspace = document.getElementById("Backspace");
-// backspace.onclick = function () {
+// backspace.onmousedown = function () {
 //   display.innerHTML = tempTxt + keyCode:"8";
 //   tempTxt = tempTxt + keyCode: "8";
 // };
 // let backspace = document.getElementById("Backspace");
-// backspace.onclick = function () {
+// backspace.onmousedown = function () {
 // display.innerHTML =  tempTxt.substring(0, tempTxt.length - 1);
 // };
 
 // let backspace = document.getElementById("Backspace");
-// backspace.onclick = function () {
+// backspace.onmousedown = function () {
 // tempTxt.slice(0, -1)
 //   console.log(tempTxt)
 // };
 
 // let backspace = document.getElementById("Backspace");
-// backspace.onclick = function () {
+// backspace.onmousedown = function () {
 //   display.innerHTML = tempTxt.substring(
 //     0,
 //   tempTxt.length-1 );
@@ -249,7 +257,7 @@ spacebar.onclick = function () {
 // Apostrophe
 
 // let apostrophe = document.getElementById("apostrophe");
-// apostrophe.onclick = function () {
+// apostrophe.onmousedown = function () {
 //   display.innerHTML = tempTxt + "ok";
 //   tempTxt = tempTxt + "ok";
 // };
