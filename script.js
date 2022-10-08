6bfed2b1be170b5cc36333df6b34798be9a564af
Kera <106094452+Keremesh@users.noreply.github.com>
2022-10-09 01:14:54 +0600

    first draft

---
+++ script.js
@@ -0,0 +1,255 @@
+// Display
+let display = document.getElementById("display");
+
+let tempTxt = "";
+
+let A = document.getElementById("A");
+A.onclick = function () {
+  display.innerHTML = tempTxt + "A";
+  tempTxt = tempTxt + "A";
+};
+
+let B = document.getElementById("B");
+B.onclick = function () {
+  display.innerHTML = tempTxt + "B";
+  tempTxt = tempTxt + "B";
+};
+
+let C = document.getElementById("C");
+C.onclick = function () {
+  display.innerHTML = tempTxt + "C";
+  tempTxt = tempTxt + "C";
+};
+
+let D = document.getElementById("D");
+D.onclick = function () {
+  display.innerHTML = tempTxt + "D";
+  tempTxt = tempTxt + "D";
+};
+let E = document.getElementById("E");
+E.onclick = function () {
+  display.innerHTML = tempTxt + "E";
+  tempTxt = tempTxt + "E";
+};
+
+let F = document.getElementById("F");
+F.onclick = function () {
+  display.innerHTML = tempTxt + "F";
+  tempTxt = tempTxt + "F";
+};
+
+let G = document.getElementById("G");
+G.onclick = function () {
+  display.innerHTML = tempTxt + "G";
+  tempTxt = tempTxt + "G";
+};
+
+let H = document.getElementById("H");
+H.onclick = function () {
+  display.innerHTML = tempTxt + "H";
+  tempTxt = tempTxt + "H";
+};
+
+let I = document.getElementById("I");
+I.onclick = function () {
+  display.innerHTML = tempTxt + "I";
+  tempTxt = tempTxt + "I";
+};
+
+let J = document.getElementById("J");
+J.onclick = function () {
+  display.innerHTML = tempTxt + "J";
+  tempTxt = tempTxt + "J";
+};
+
+let K = document.getElementById("K");
+K.onclick = function () {
+  display.innerHTML = tempTxt + "K";
+  tempTxt = tempTxt + "K";
+};
+
+let L = document.getElementById("L");
+L.onclick = function () {
+  display.innerHTML = tempTxt + "L";
+  tempTxt = tempTxt + "L";
+};
+
+let M = document.getElementById("M");
+M.onclick = function () {
+  display.innerHTML = tempTxt + "M";
+  tempTxt = tempTxt + "M";
+};
+
+let N = document.getElementById("N");
+N.onclick = function () {
+  display.innerHTML = tempTxt + "N";
+  tempTxt = tempTxt + "N";
+};
+
+let O = document.getElementById("O");
+O.onclick = function () {
+  display.innerHTML = tempTxt + "O";
+  tempTxt = tempTxt + "O";
+};
+
+let P = document.getElementById("P");
+P.onclick = function () {
+  display.innerHTML = tempTxt + "P";
+  tempTxt = tempTxt + "P";
+};
+
+let Q = document.getElementById("Q");
+Q.onclick = function () {
+  display.innerHTML = tempTxt + "Q";
+  tempTxt = tempTxt + "Q";
+};
+
+let R = document.getElementById("R");
+R.onclick = function () {
+  display.innerHTML = tempTxt + "R";
+  tempTxt = tempTxt + "R";
+};
+
+let S = document.getElementById("S");
+S.onclick = function () {
+  display.innerHTML = tempTxt + "S";
+  tempTxt = tempTxt + "S";
+};
+
+let T = document.getElementById("T");
+T.onclick = function () {
+  display.innerHTML = tempTxt + "T";
+  tempTxt = tempTxt + "T";
+};
+
+let U = document.getElementById("U");
+U.onclick = function () {
+  display.innerHTML = tempTxt + "U";
+  tempTxt = tempTxt + "U";
+};
+
+let V = document.getElementById("V");
+V.onclick = function () {
+  display.innerHTML = tempTxt + "V";
+  tempTxt = tempTxt + "V";
+};
+
+let W = document.getElementById("W");
+W.onclick = function () {
+  display.innerHTML = tempTxt + "W";
+  tempTxt = tempTxt + "W";
+};
+
+let X = document.getElementById("X");
+X.onclick = function () {
+  display.innerHTML = tempTxt + "X";
+  tempTxt = tempTxt + "X";
+};
+
+let Y = document.getElementById("Y");
+Y.onclick = function () {
+  display.innerHTML = tempTxt + "Y";
+  tempTxt = tempTxt + "Y";
+};
+
+let Z = document.getElementById("Z");
+Z.onclick = function () {
+  display.innerHTML = tempTxt + "Z";
+  tempTxt = tempTxt + "Z";
+};
+
+//Numbers
+let n1 = document.getElementById("1");
+n1.onclick = function () {
+  display.innerHTML = tempTxt + "1";
+  tempTxt = tempTxt + "1";
+};
+
+let n2 = document.getElementById("2");
+n2.onclick = function () {
+  display.innerHTML = tempTxt + "2";
+  tempTxt = tempTxt + "2";
+};
+
+let n3 = document.getElementById("3");
+n3.onclick = function () {
+  display.innerHTML = tempTxt + "3";
+  tempTxt = tempTxt + "3";
+};
+let n4 = document.getElementById("4");
+n4.onclick = function () {
+  display.innerHTML = tempTxt + "4";
+  tempTxt = tempTxt + "4";
+};
+
+let n5 = document.getElementById("5");
+n5.onclick = function () {
+  display.innerHTML = tempTxt + "5";
+  tempTxt = tempTxt + "5";
+};
+
+let n6 = document.getElementById("6");
+n6.onclick = function () {
+  display.innerHTML = tempTxt + "6";
+  tempTxt = tempTxt + "6";
+};
+
+let n7 = document.getElementById("7");
+n7.onclick = function () {
+  display.innerHTML = tempTxt + "7";
+  tempTxt = tempTxt + "7";
+};
+
+let n8 = document.getElementById("8");
+n8.onclick = function () {
+  display.innerHTML = tempTxt + "8";
+  tempTxt = tempTxt + "8";
+};
+let n9 = document.getElementById("9");
+n9.onclick = function () {
+  display.innerHTML = tempTxt + "9";
+  tempTxt = tempTxt + "9";
+};
+
+// Space
+let spacebar = document.getElementById("spacebar");
+spacebar.onclick = function () {
+  display.innerHTML = tempTxt + "\n";
+  tempTxt += "\n";
+};
+// let spacebar = document.getElementById("spacebar");
+// spacebar.onclick = function () {
+// display.innerHTML = tempTxt + ' ';
+// };
+
+// Backspace
+// let backspace = document.getElementById("Backspace");
+// backspace.onclick = function () {
+//   display.innerHTML = tempTxt + keyCode:"8";
+//   tempTxt = tempTxt + keyCode: "8";
+// };
+// let backspace = document.getElementById("Backspace");
+// backspace.onclick = function () {
+// display.innerHTML =  tempTxt.substring(0, tempTxt.length - 1);
+// };
+
+// let backspace = document.getElementById("Backspace");
+// backspace.onclick = function () {
+// tempTxt.slice(0, -1)
+//   console.log(tempTxt)
+// };
+
+// let backspace = document.getElementById("Backspace");
+// backspace.onclick = function () {
+//   display.innerHTML = tempTxt.substring(
+//     0,
+//   tempTxt.length-1 );
+// }
+
+// Apostrophe
+
+// let apostrophe = document.getElementById("apostrophe");
+// apostrophe.onclick = function () {
+//   display.innerHTML = tempTxt + "ok";
+//   tempTxt = tempTxt + "ok";
+// };
